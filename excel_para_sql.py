2722527112e5cbcb61e406ef98a2cbc179784c46
dangelo-dev <victordangelo00@gmail.com>
2024-05-21 14:52:45 -0300

    ADD: Tratamento de exceção para não incluir dados duplicados

--- excel_para_sql.py
+++ excel_para_sql.py
@@ -1,13 +1,14 @@
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, exc, text
 
 def abrir_planilha(loja_id, arquivo):
     df = pd.read_excel('7087.xlsx', skiprows=10, usecols=[1,4,5,6,7])
-    df = df[:-1] # Linha contendo o total acumulado
-    df.columns = ['Data', 'Boletos', 'Itens', 'Vr. Liquido', 'Desconto']
+    df = df[:-1] # Excluindo a ultima linha que contém o total acumulado
+    df.columns = ['Data', 'Boletos', 'Itens', 'Venda_Liquida', 'Desconto']
     df.insert(0, 'Loja', loja_id)
     return df
 
+# lista de lojas e arquivos correspondentes
 lojas = {
     '7087': '7087.xlsx',
     '7162': '7162.xlsx',
@@ -29,8 +30,26 @@ lojas = {
 
 engine = create_engine('sqlite:///vendas_lojas.db')
 
+criar_tabela = """
+    CREATE TABLE IF NOT EXISTS vendas_lojas (
+        Loja TEXT NOT NULL,
+        Data DATE NOT NULL,
+        Boletos INTEGER NOT NULL,
+        Itens INTEGER NOT NULL,
+        Venda_Liquida REAL NOT NULL,
+        Desconto REAL,
+        UNIQUE (Loja, Data, Boletos)
+    )
+"""
+
+with engine.connect() as conn:
+    conn.execute(text(criar_tabela))
+    
 for loja_id, arquivo in lojas.items():
     df_result = abrir_planilha(loja_id, arquivo)
-    df_result.to_sql('vendas', con=engine, if_exists='append', index=False)
+    try:
+        df_result.to_sql('vendas_lojas', con=engine, if_exists='append', index=False)
+    except exc.IntegrityError as e:
+        print(f"Erro ao inserir dados para a loja {loja_id}: {e.orig}")
 
 print('Dados inseridos com sucesso!')
